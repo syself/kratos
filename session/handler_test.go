67f03648c66f2728d6e32821a3c0fe5ad7240b5c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-08-19 20:10:14 +0200

    feat: add MFA enforcment option to whoami and settings

--- session/handler_test.go
+++ session/handler_test.go
@@ -71,7 +71,7 @@ func TestSessionWhoAmI(t *testing.T) {
 		}
 
 		t.Run("case=aal2-aal2", func(t *testing.T) {
-			conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+			conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 			run(t, "aal2-aal2", http.StatusOK)
 		})
 
@@ -81,7 +81,7 @@ func TestSessionWhoAmI(t *testing.T) {
 		})
 
 		t.Run("case=aal2-aal1", func(t *testing.T) {
-			conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+			conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 			body := run(t, "aal2-aal1", http.StatusForbidden)
 			assert.EqualValues(t, ErrAALNotSatisfied.Reason(), gjson.Get(body, "error.reason").String(), body)
 		})
@@ -92,7 +92,7 @@ func TestSessionWhoAmI(t *testing.T) {
 		})
 
 		t.Run("case=aal1-aal1", func(t *testing.T) {
-			conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+			conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 			run(t, "aal1-aal1", http.StatusOK)
 		})
 	})
@@ -190,12 +190,12 @@ func TestSessionWhoAmI(t *testing.T) {
 				}
 
 				t.Run("fulfilled for aal2 if identity has aal2", func(t *testing.T) {
-					conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+					conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 					run(t, []identity.CredentialsType{identity.CredentialsTypePassword, identity.CredentialsTypeWebAuthn}, 200, &idAAL2)
 				})
 
 				t.Run("rejected for aal1 if identity has aal2", func(t *testing.T) {
-					conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+					conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 					run(t, []identity.CredentialsType{identity.CredentialsTypePassword}, 403, &idAAL2)
 				})
 
@@ -205,12 +205,12 @@ func TestSessionWhoAmI(t *testing.T) {
 				})
 
 				t.Run("fulfilled for aal2 if identity has aal1", func(t *testing.T) {
-					conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+					conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 					run(t, []identity.CredentialsType{identity.CredentialsTypePassword, identity.CredentialsTypeWebAuthn}, 200, &idAAL1)
 				})
 
 				t.Run("fulfilled for aal1 if identity has aal1", func(t *testing.T) {
-					conf.MustSet(config.ViperKeySessionWhoAmIAAL, "highest_available")
+					conf.MustSet(config.ViperKeySessionWhoAmIAAL, config.HighestAvailableAAL)
 					run(t, []identity.CredentialsType{identity.CredentialsTypePassword}, 200, &idAAL1)
 				})
 			})
